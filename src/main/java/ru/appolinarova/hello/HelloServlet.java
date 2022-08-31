d745841016a8641324bd72b08f7f0cde945d39eb
Anastasia Appolinarova <anastasiia_appolinarova@epam.com>
2022-08-31 20:16:36 +0300

    commit my app

---
+++ src/main/java/ru/appolinarova/hello/HelloServlet.java
@@ -0,0 +1,25 @@
+package ru.appolinarova.hello;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter writer = null;
+        try {
+            writer = response.getWriter();
+            writer.println("<h2>Hello World</h2>");
+        } finally {
+            writer.close();
+        }
+    }
+}
---
+++ src/test/java/ru/appolinarova/hello/SimpleTest.java
@@ -0,0 +1,13 @@
+package ru.appolinarova.hello;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class SimpleTest {
+
+    @Test
+    public void simpleTest() {
+        assertTrue(true);
+    }
+}
